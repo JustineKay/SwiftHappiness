1bdee429ff2cf6fc79220e7360bd9b6cbc66fb10
Justine Gartner <GartnerJustine@gmail.com>
2016-05-29 12:49:55 -0400

    Set up Face View and HappinessVC

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,17 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Justine Kay on 5/29/16.
+//  Copyright © 2016 Justine Kay. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+    
+    override func drawRect(rect: CGRect) {
+        
+    }
+    
+}
---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Justine Kay on 5/29/16.
+//  Copyright © 2016 Justine Kay. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+    
+}
