3d699a985283dfb68b129aa489d9b0c18708a50e
Justine Gartner <GartnerJustine@gmail.com>
2016-05-29 13:02:31 -0400

    Drawing facePath successfully

--- Happiness/FaceView.swift
+++ Happiness/FaceView.swift
@@ -8,10 +8,30 @@
 
 import UIKit
 
-class FaceView: UIView {
+class FaceView: UIView
+{
+    var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay() } }
+    var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay() } }
     
-    override func drawRect(rect: CGRect) {
+    var faceCenter: CGPoint {
+        return convertPoint(center, toView: superview)
+    }
+    
+    var faceRadius:CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2
+    }
+    
+        override func drawRect(rect: CGRect)
+    {
+        let facePath = UIBezierPath(arcCenter: faceCenter,
+                                    radius: faceRadius,
+                                    startAngle: 0,
+                                    endAngle: CGFloat(2*M_PI),
+                                    clockwise: true)
         
+        facePath.lineWidth = lineWidth
+        color.set()
+        facePath.stroke()
     }
     
 }
